005a03e35cdd2849907d4d6e57f556c77874e2c9
harsha-ys <yoshith1996@gmail.com>
2025-05-13 20:00:09 +0530

    Initializing

--- create a rest controller/learning/src/main/java/com/learning/learning/LearningApplication.java
+++ create a rest controller/learning/src/main/java/com/learning/learning/LearningApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningApplication {
-
+    //added a comment
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
 	}
