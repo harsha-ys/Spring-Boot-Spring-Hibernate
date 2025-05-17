9dc69630af18511d6154a7766a2fcaf329b3cd36
harsha-ys <yoshith1996@gmail.com>
2025-05-17 14:40:37 +0530

    say hello world

---
+++ create a rest controller/learning/src/main/java/com/learning/learning/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.learning.learning.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHellow(){
+        return "Hellow World";
+    }
+
+}
