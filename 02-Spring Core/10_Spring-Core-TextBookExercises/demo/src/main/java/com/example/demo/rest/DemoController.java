4699d8b775765c3fae3a8f272187be5bd8ae3675
yoshithsriharshana <yoshith1996@gmail.com>
2026-08-17 13:44:43 +0530

    Request parameters

--- 02-Spring Core/10_Spring-Core-TextBookExercises/demo/src/main/java/com/example/demo/rest/DemoController.java
+++ 02-Spring Core/10_Spring-Core-TextBookExercises/demo/src/main/java/com/example/demo/rest/DemoController.java
@@ -2,22 +2,21 @@ package com.example.demo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoController {
-    @Autowired
-    @Qualifier("tennisCoach")
-    private ICoach myCoach;
 
-//    @Autowired
-//    public void setMyCoach(ICoach coach){
-//        myCoach = coach;
-//    }
-    @GetMapping("/dailyWorkouts")
-    public String getDailyWorkouts(){
-        return myCoach.getDailyWorkout();
+    @RequestMapping("/home")
+    public String home(@RequestParam(required = false) String color, @RequestParam(required = false) String name, Model page){
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
+        return "home.html";
     }
 
 }
