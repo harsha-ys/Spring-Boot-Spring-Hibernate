85a583bf06388a208c9442ad8721dc89aed64c1c
yoshithsriharshana <yoshith1996@gmail.com>
2026-08-17 13:50:50 +0530

    path variables

--- 02-Spring Core/10_Spring-Core-TextBookExercises/demo/src/main/java/com/example/demo/rest/DemoController.java
+++ 02-Spring Core/10_Spring-Core-TextBookExercises/demo/src/main/java/com/example/demo/rest/DemoController.java
@@ -4,17 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DemoController {
 
-    @RequestMapping("/home")
-    public String home(@RequestParam(required = false) String color, @RequestParam(required = false) String name, Model page){
-        page.addAttribute("username", name);
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color, Model page){
+        page.addAttribute("username", "Yoshith");
         page.addAttribute("color", color);
         return "home.html";
     }
